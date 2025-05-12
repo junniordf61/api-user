0f6ebaea219db29c9df419cdbe03585afc5de88a
José Carlos <junniordf61@gmail.com>
2025-05-12 12:56:47 -0300

    alterações de perfil de usuarios

--- src/public/usuarios.js
+++ src/public/usuarios.js
@@ -2,10 +2,11 @@ const API_BASE_URL = "https://api-user-5gkk.onrender.com";
 
 document.getElementById('listarUsuarios').addEventListener('click', listarUsuarios);
 
-async function listarUsuarios() {
+async function verPerfilAposLogin() {
   const token = localStorage.getItem('token');
+
   try {
-    const res = await fetch(`${API_BASE_URL}/usuarios`, {
+    const res = await fetch(`${API_BASE_URL}/usuarios/perfil`, {
       headers: { 'Authorization': `Bearer ${token}` }
     });
 
@@ -14,16 +15,21 @@ async function listarUsuarios() {
       throw new Error(error);
     }
 
-    const usuarios = await res.json();
-    const listaUsuarios = document.getElementById('listaUsuarios');
-    listaUsuarios.innerHTML = '';
-    usuarios.forEach(user => {
-      const li = document.createElement('li');
-      li.textContent = `Nome: ${user.nome} | Email: ${user.email} | Perfil: ${user.role}`;
-      listaUsuarios.appendChild(li);
-    });
+    const data = await res.json();
+    if (data.usuario) {
+      document.getElementById('formContainer').classList.add('hidden');
+      document.getElementById('perfilContainer').classList.remove('hidden');
+      document.getElementById('perfilInfo').innerHTML = `
+        <p><strong>Nome:</strong> ${data.usuario.nome}</p>
+        <p><strong>Email:</strong> ${data.usuario.email}</p>
+        <p><strong>Role:</strong> ${data.usuario.role}</p>
+      `;
+    } else {
+      alert('Erro ao carregar o perfil');
+    }
   } catch (error) {
-    console.error('Erro ao listar usuários:', error.message);
-    alert('Erro ao listar usuários: ' + error.message);
+    console.error('Erro ao carregar o perfil:', error.message);
+    alert('Erro ao comunicar com o servidor: ' + error.message);
   }
 }
+
